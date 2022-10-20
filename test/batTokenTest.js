9ffcbc7767c88d7c6a4e7f005c353a1212da8ca1
Subrat chandra Naha <89215102+subrat0018@users.noreply.github.com>
2022-10-21 00:50:02 +0530

    totalSupply is set to 1 million and tested

--- test/batTokenTest.js
+++ test/batTokenTest.js
@@ -1,9 +1,21 @@
 const BatToken = artifacts.require("BatToken");
 contract("BatToken",()=>{
-    it("The Name of the Token is Bat Token",async ()=>
+    it("The Name of the Token must be Bat Token",async ()=>
     {
         let batToken = await BatToken.deployed();
         let name = await batToken.name();
-        assert(name === "Bat Token" , `The name of the token must be Bat Token but found ${name}`);
+        assert(name === "Bat Token" , `The name of the token must be 'Bat Token' but found ${name}`);
+    });
+    it("The symbol of the Token must be BAT",async ()=>
+    {
+        let batToken = await BatToken.deployed();
+        let symbol = await batToken.symbol();
+        assert(symbol === "BAT",`The symbol of the Token must be 'BAT' but found ${symbol} `);
+    });
+    it("The total Supply must 1 million",async ()=>
+    {
+        let batToken = await BatToken.deployed();
+        let totalSupply = await batToken.totalSupply();
+        assert(totalSupply.toNumber() === 1000000,`total supply must be 1000000 but found but found ${totalSupply}`);
     })
 });
